285c44bdef57d61c21dea7d2b62d7aaa9059e5e4
guduxueyu <scjy@gengyuanfeng.local>
2016-03-04 10:38:25 +0800

    稳定版本

--- MyFristApp/ViewController.m
+++ MyFristApp/ViewController.m
@@ -17,7 +17,13 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // 1.0已经完成
-    
+    /*
+     项目上传github
+     1、准备好要上传的项目
+     2、点击加号->add，让项目处于准备上传仓库状态
+     3、写好简介和详细描述，将项目提交到本地仓库
+     4、点击
+     */
 }
 
 - (void)didReceiveMemoryWarning {
