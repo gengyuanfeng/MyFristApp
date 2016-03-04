84a395b20ee40bde58071e7ccc6f4fd4213e0490
guduxueyu <scjy@gengyuanfeng.local>
2016-03-04 10:07:08 +0800

    1.0已经完成

---
+++ MyFristApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyFristApp
+//
+//  Created by scjy on 16/3/4.
+//  Copyright © 2016年 耿远风. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MyFristApp/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  MyFristApp
+//
+//  Created by scjy on 16/3/4.
+//  Copyright © 2016年 耿远风. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // 1.0已经完成
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
